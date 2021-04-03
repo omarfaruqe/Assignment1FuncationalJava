34c1c9e2eaa1c871ea4d9d1ab3cbb6f15fb37782
Omar Faruqe <faruqe.cse@gmail.com>
2021-04-03 14:15:19 +0600

    Test Class Added for Unit Test using JUnit5

---
+++ src/com/cseru/Assignment1FunctionalJavaTest.java
@@ -0,0 +1,43 @@
+package com.cseru;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class Assignment1FunctionalJavaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void factorial() {
+    }
+
+    @Test
+    void indexOfFirstOccurrence() {
+    }
+
+    @Test
+    void yearsTilOneMillion() {
+    }
+
+    @Test
+    void printInReverse() {
+    }
+
+    @Test
+    void findRange() {
+    }
+
+    @Test
+    void diceRoll() {
+    }
+
+    @Test
+    void monopolyRoll() {
+    }
+}
